bf1a1a77eef11143ecf711a311591a9dbb00db87
Alain <alain@gmail.com>
2025-07-19 19:49:00 -0500

    avence hasta ahora de mi proyecto

--- ReservaCanchasUNA/views.py
+++ ReservaCanchasUNA/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+import datetime
+from . import forms, models
 
 
 def inicio(request):
     return render(request, 'ReservaCanchasUNA/inicio.html')
+
+def registro(request):
+    return render(request, 'ReservaCanchasUNA/registrarse.html')
+
+def registro(request):
+    formulario = forms.Formulario_Persona()
+    return render(request, 'ReservaCanchasUNA/registrarse.html', {
+        "formulario": formulario
+    })
+
+##Para crear los registros
+def crear_persona(request):
+   
+    if(request.method == "POST"):
+        formulario = forms.Formulario_Persona(request.POST)
+        if(formulario.is_valid()):
+            nombres = formulario.cleaned_data['nombre']
+            apellidos = formulario.cleaned_data['apellidos']
+            persona = models.Persona.objects.create(nombre = nombres, apellidos = apellidos)
+            persona.save()
+            return HttpResponse("Hola " + nombres + " " + apellidos + ", fuiste registrado correctamente.")
+            #return HttpResponse(models.Persona.objects.all())
+    formulario = forms.Formulario_Persona()
+    return render(request, 'ReservaCanchasUNA/registrarse.html', {
+        "formulario": formulario
+    })
