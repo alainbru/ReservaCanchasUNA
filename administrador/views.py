099ebbae0507336f104128a5910a34a92f9d8e7f
Alain <alain@gmail.com>
2025-07-21 22:08:15 -0500

    agrege el template en app admin para ver la lista de ususarios

--- administrador/views.py
+++ administrador/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from ReservaCanchasUNA.models import Persona #importamos la base desde el principal
 
 def inicio_ad(request):
     return render(request, 'administrador/inicio_admin.html')
+
+def lista_usuarios(request):
+    personas = Persona.objects.all()  # Traes todos los registros de Persona
+    return render(request, 'administrador/lista_usuarios.html', {'personas': personas})
