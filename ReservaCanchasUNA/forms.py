36197db59af842793cc0eb7f0287d65eff654340
unknown <pomamaqueraabad[1>
2025-07-22 09:42:38 -0500

    prueba 1

--- ReservaCanchasUNA/forms.py
+++ ReservaCanchasUNA/forms.py
@@ -1,14 +1,36 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Formulario_Persona(forms.Form):
+    ROLES_CHOICES = [
+        ('estudiante', 'Estudiante'),
+        ('coordinador', 'Coordinador'),
+        ('admin', 'Administrador'),
+    ]
     nombre = forms.CharField(max_length=100, label="Nombre")
     apellido_paterno = forms.CharField(max_length=100, label="Apellido Paterno")
     apellido_materno = forms.CharField(max_length=100, label="Apellido Materno")
-    correo= forms.EmailField(max_length=100, required=False, label="Correo Electrónico")
+    correo = forms.EmailField(
+        label="Correo Institucional",
+        help_text="Debe ser un correo @est.unap.edu.pe"
+    )
+    def clean_correo(self):
+        correo = self.cleaned_data['correo']
+        if not correo.endswith('@est.unap.edu.pe'):
+            raise ValidationError("Solo se permiten correos de la UNA DNI(@est.unap.edu.pe)")
+        return correo
     escuela = forms.CharField(max_length=100, required=False, label="Escuela")
-    codigo = forms.CharField(max_length=100, required=False, label="Código")
+    codigo = forms.IntegerField(
+        required=True, 
+        label="Código",
+        validators=[
+            MinValueValidator(100000, message="El código debe tener 6 dígitos."),
+            MaxValueValidator(999999, message="El código debe tener 6 dígitos.")
+        ]
+    )
     contrasena = forms.CharField(max_length=100, widget=forms.PasswordInput, required=False, label="Contraseña")
-    rol = forms.CharField(max_length=100, required=False, label="Rol")
+    rol = forms.ChoiceField(choices=ROLES_CHOICES, label="Rol")
     
 class Formulario_Login(forms.Form):
     codigo = forms.CharField(label="Codigo")
--- ReservaCanchasUNA/models.py
+++ ReservaCanchasUNA/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator, EmailValidator
 
 # Create your models here.
 class Persona(models.Model):
@@ -7,9 +8,18 @@ class Persona(models.Model):
     apellido_materno = models.CharField(max_length=100, blank=True, null=True)  
     correo = models.EmailField(max_length=100, blank=True, null=True)
     escuela = models.CharField(max_length=100, blank=True, null=True)
-    codigo = models.CharField(max_length=100, blank=True, null=True)
+    codigo = models.IntegerField(
+        blank=True, # Considera cambiar a False si siempre debe ser obligatorio
+        null=True,  # Considera cambiar a False si siempre debe ser obligatorio
+        validators=[
+            MinValueValidator(100000, message="El código debe tener 6 dígitos."),
+            MaxValueValidator(999999, message="El código debe tener 6 dígitos.")
+        ]
+    )
     contrasena = models.CharField(max_length=100, blank=True, null=True)
-    rol = models.CharField(max_length=100, blank=True, null=True)
+    rol = models.CharField(max_length=100, default='estudiante', null=False)
+    def __str__(self):
+        return f"{self.nombre} {self.apellido_paterno} ({self.rol})"
 
 
 class Reserva(models.Model):
