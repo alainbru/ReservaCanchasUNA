6da421591eb96302020fac930e35592788ddc2e9
Alain <alain@gmail.com>
2025-07-22 21:42:22 -0500

    boton pra ver reservas en cooridnaor

--- coordinador/urls.py
+++ coordinador/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from coordinador import views
+from ReservaCanchasUNA.views import vista_reservas
+
 
 app_name = 'coordinador'
 
 
 urlpatterns = [
     path('', views.inicio_cor, name='inicio_coordinador'),
+    path('reservas/', vista_reservas, name='coordinador_reservas'),
+
 
 ]
