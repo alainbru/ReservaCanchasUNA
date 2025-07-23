2f87775c6dfbb28e72161ac19cb3ee05a7acb72c
unknown <pomamaqueraabad[1>
2025-07-22 22:56:48 -0500

    agrege seleccion de escuela en register

--- ReservaCanchasUNA/forms.py
+++ ReservaCanchasUNA/forms.py
@@ -8,6 +8,41 @@ class Formulario_Persona(forms.Form):
         ('coordinador', 'Coordinador'),
         ('admin', 'Administrador'),
     ]
+    CARRERAS_CHOICES = [
+        ('agronomia', 'Agronomía'),
+        ('antropologia', 'Antropología'),
+        ('arquitectura', 'Arquitectura y Urbanismo'),
+        ('biologia', 'Biología'),
+        ('ciencias_contables', 'Ciencias Contables'),
+        ('ciencias_comunicacion', 'Ciencias de la Comunicación Social'),
+        ('ciencias_fisicas_matematicas', 'Ciencias Físicas y Matemáticas'),
+        ('ciencias_juridicas_politicas', 'Ciencias Jurídicas y Políticas'),
+        ('ciencias_sociales', 'Ciencias Sociales'),
+        ('educacion_inicial', 'Educación Inicial'),
+        ('educacion_primaria', 'Educación Primaria'),
+        ('educacion_secundaria', 'Educación Secundaria'),
+        ('enfermeria', 'Enfermería'),
+        ('estadistica_informatica', 'Estadística e Informática'),
+        ('farmacia_bioquimica', 'Farmacia y Bioquímica'),
+        ('ingenieria_agricola', 'Ingeniería Agrícola'),
+        ('ingenieria_agroindustrial', 'Ingeniería Agroindustrial'),
+        ('ingenieria_civil', 'Ingeniería Civil'),
+        ('ingenieria_electronica', 'Ingeniería Electrónica'),
+        ('ingenieria_estadistica', 'Ingeniería Estadística'),
+        ('ingenieria_geologica', 'Ingeniería Geológica'),
+        ('ingenieria_minas', 'Ingeniería de Minas'),
+        ('ingenieria_metalurgica', 'Ingeniería Metalúrgica'),
+        ('ingenieria_quimica', 'Ingeniería Química'),
+        ('ingenieria_sistemas', 'Ingeniería de Sistemas'),
+        ('medicina_humana', 'Medicina Humana'),
+        ('medicina_veterinaria', 'Medicina Veterinaria y Zootecnia'),
+        ('nutricion_humana', 'Nutrición Humana'),
+        ('odontologia', 'Odontología'),
+        ('psicologia', 'Psicología'),
+        ('sociologia', 'Sociología'),
+        ('turismo', 'Turismo'),
+        # Puedes añadir más carreras aquí
+    ]
     nombre = forms.CharField(max_length=100, label="Nombre")
     apellido_paterno = forms.CharField(max_length=100, label="Apellido Paterno")
     apellido_materno = forms.CharField(max_length=100, label="Apellido Materno")
@@ -24,7 +59,7 @@ class Formulario_Persona(forms.Form):
         if Persona.objects.filter(correo=correo).exists():
             raise ValidationError("Este correo ya está registrado. Por favor, usa otro.")
         return correo
-    escuela = forms.CharField(max_length=100, required=False, label="Escuela")
+    escuela = forms.ChoiceField(choices=CARRERAS_CHOICES, label="Escuela Profesional")
     codigo = forms.IntegerField(
         required=True, # Considera cambiar a True si siempre debe ser obligatorio
         label="Código",
